a456edfa70b48d36d02bd7c252e299d2e5d7c2ac
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-02-10 20:35:06 +0530

    Started

---
+++ App.js
@@ -0,0 +1,7 @@
+/* eslint-disable react-native/no-inline-styles */
+import {View} from 'react-native';
+
+export default function App() {
+  // eslint-disable-next-line react/react-in-jsx-scope
+  return <View style={{flex: 1, backgroundColor: 'plum'}} />;
+}
